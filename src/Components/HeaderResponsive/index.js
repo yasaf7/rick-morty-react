c21c8681f293150a3dd328bb5d840d866856e008
yasaf7 <yasminramos5566@gmail.com>
2021-11-30 03:20:13 -0400

    Refatoração das rotas no Menu

--- src/Components/HeaderResponsive/index.js
+++ src/Components/HeaderResponsive/index.js
@@ -20,9 +20,9 @@ function HeaderResposive() {
 
             <ul className={`menu-options ${active ? ' active' : ''}`}>
                 <li><a href="/">Home</a></li>
-                <li><a href="/">Personagens</a></li>
+                <li><a href="/personagens">Personagens</a></li>
                 <li><a href="/">Episódios</a></li>
-                <li><a href="/">Sobre</a></li>
+                <li><a href="/sobre">Sobre</a></li>
 
             </ul>
         </div>
