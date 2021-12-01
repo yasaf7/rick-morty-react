28be3b0dfd140b77e2889cf4fa1a43055fea3796
yasaf7 <yasminramos5566@gmail.com>
2021-11-30 22:08:51 -0400

    Ajustando rotas para requisição da api

--- src/Api.js
+++ src/Api.js
@@ -1,11 +1,27 @@
 import axios from 'axios';
+const BASE_URL = "https://rickandmortyapi.com/api/";
 
-const api = {
-    getPersonagemByName: async () => {
-        const response = await axios.get("https://rickandmortyapi.com/api/character/2");
+async function basicFetch(endpoint) {
+    const URL = BASE_URL + endpoint;
+    let data = null;
 
-        return response;
+    try {
+        const response = await axios.get(URL);
+        data = response.data;
+    } catch (error) {
+        data = error.response.data;
     }
+    return data;
+}
+const api = {
+    getPersonagemByName: async (name) => {
+        const res = await basicFetch(`/character/?name=${name}`)
+        return res;
+    },
+    getPersonagemById: async (id) => {
+        const res = await basicFetch(`/character/${id}`)
+        return res;
+    },
 }
 
 export default api;
